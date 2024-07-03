49826ba14fc757d90b47777b00447bfe65e65f35
Bob (ZubovaAR) <bob@example.com>
2024-07-03 08:23:44 +0300

    Добавление произведения

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ cout << "Enter A and B: ";
 int a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
+     << "A - B = " << a - b << '\n'
+    << "A * B = " << a * b << '\n';
 }
